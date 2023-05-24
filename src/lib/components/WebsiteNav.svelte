1c1d3ad57af3638feb414b1ee96eb9fcea7de1a1
michael <michael@letsken.com>
2023-05-24 10:40:01 +0200

    [WIP] New menu.

--- src/lib/components/WebsiteNav.svelte
+++ src/lib/components/WebsiteNav.svelte
@@ -4,9 +4,10 @@
   import PrimaryButton from './PrimaryButton.svelte';
   export let editable = false;
   export let currentUser;
-
   export let showUserMenu = undefined;
   export let isDark = undefined;
+
+  let showMenu = false;
 
   function onKeyDown(e) {
     // Turn on editing
@@ -27,35 +28,33 @@
   <div class="max-w-screen-md mx-auto py-4 px-6">
     <NotEditable {editable}>
       <div class="flex items-center relative">
+        <a href="/" class="text-sm font-bold uppercase">Michael Aufreiter</a>
         <div class="flex-1" />
         <PrimaryButton size="sm" href="/posts/new">New letter</PrimaryButton>
         {#if currentUser}
           <button
-            on:click={() => (showUserMenu = !showUserMenu)}
+            on:click={() => (showMenu = !showMenu)}
             class="ml-0 px-4"
             title={currentUser.name}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
-              />
-            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>          
           </button>
         {/if}
-        <div class="flex-1" />
       </div>
     </NotEditable>
   </div>
 </div>
+{/if}
+
+{#if showMenu}
+  <div class="bg-white absolute inset-0 z-50">
+    <div class="max-w-screen-md mx-auto py-4 px-6 flex flex-col">
+      <a class="text-3xl font-bold" href="/">About</a>
+      <a class="text-3xl font-bold" href="/">Letters</a>
+    </div>
+  </div>
 {/if}
 
 <svelte:window on:keydown={onKeyDown} />
