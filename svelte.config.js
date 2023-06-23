e4e8bb6c01d2c1010573d9f762a67522b607d876
Tom Atkins <tom@keybits.net>
2023-06-23 08:35:49 +0100

    Add comment explaining our use of envPrefix

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,18 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
   kit: {
     adapter: adapter({
-     envPrefix: 'PUBLIC_' // allow us to use ORIGIN in client-side code
+      /**
+       * We are setting sveltekit's adapter-node envPrefix to PUBLIC_
+       * as a bit of a hack to help prevent repetition in our deployment
+       * configuration. Our application code makes use of the deployed
+       * URL in several places that are compiled into client side code.
+       * sveltekit also uses this value, normally as ORIGIN see
+       * https://kit.svelte.dev/docs/adapter-node#environment-variables-origin-protocol-header-and-host-header
+       * By changing ORIGIN to PUBLIC_ORIGIN we're able to reuse the value
+       * where it needs to be compiled to client side code at build time, see:
+       * https://kit.svelte.dev/docs/modules#$env-dynamic-public
+       */
+     envPrefix: 'PUBLIC_'
      }),
     csrf: {
       checkOrigin: false
