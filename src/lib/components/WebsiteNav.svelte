09b359d93dd953d0723ac5ec75951da0d4a4e9d9
Tom Atkins <tom@keybits.net>
2023-07-26 13:48:30 +0100

    Fix indenting

--- src/lib/components/WebsiteNav.svelte
+++ src/lib/components/WebsiteNav.svelte
@@ -43,24 +43,24 @@
         <div class="flex-1" />
         <button on:click={() => (showMenu = true)} class="ml-0 pl-4" title={'Open Menu'}>
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
-        </button>
-      </div>
-    </NotEditable>
+          <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+    </div>
+  </NotEditable>
   </div>
-</div>
+  </div>
 
 
 {#if showMenu && !editable}
