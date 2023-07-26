6b705776d6f373454fa6a47f26f4b2dddad705a5
Tom Atkins <tom@keybits.net>
2023-07-26 13:18:15 +0100

    Update words for clarity and ease of use

--- src/routes/friends/+page.svelte
+++ src/routes/friends/+page.svelte
@@ -24,7 +24,7 @@
 <div class="max-w-screen-md mx-auto px-6 pb-8">
   <h1 class="text-center font-bold text-2xl sm:text-4xl pt-14">{data.friends.length} Friends</h1>
   <p class="text-center sm:text-xl pb-4">
-    Assign nicknames to your contacts, and keep the data up to date.
+    Contact names auto-complete when you share letters.
   </p>
   <div class="flex justify-center pb-14">
     <PrimaryButton size="sm" href="/friends/new">Add friend</PrimaryButton>
