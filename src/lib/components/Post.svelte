88bd72f08c81bdfe0001414515641eb956e8026c
michael <michael@letsken.com>
2023-07-27 11:39:09 +0200

    Focus the title field when creating a new letter.

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -3,10 +3,20 @@
   import RichText from '$lib/components/RichText.svelte';
   import { formatDate } from '$lib/util';
   import NotEditable from './NotEditable.svelte';
+  import { onMount } from 'svelte';
+
   export let title;
   export let content;
   export let created_at;
   export let editable;
+
+  let titleEl;
+  // HACK: set the focus to the title element, this will only work for new letters, which is intended behavior
+  onMount(() => {
+    setTimeout(() => {
+      titleEl.querySelector('.ProseMirror')?.focus();
+    }, 500);
+  });
 </script>
 
 <div>
@@ -14,7 +24,7 @@
     <NotEditable {editable}>
       <div class="pb-2 sm:text-lg">{formatDate(created_at)}</div>
     </NotEditable>
-    <h1 class="text-3xl md:text-5xl font-bold">
+    <h1 bind:this={titleEl} class="text-3xl md:text-5xl font-bold">
       <PlainText {editable} bind:content={title} placeholder="Letter title…"/>
     </h1>
   </div>
