729e32bc3f476dd3d0038eb1f97aca7c3e17d4bc
michael <michael@letsken.com>
2023-09-11 16:57:54 +0200

    Derive origin from url.origin and pass to client.

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,7 +1,9 @@
-export async function load({ locals }) {
+export async function load({ locals, url }) {
+
   return {
     currentUser: locals.currentUser,
     bio: locals.bio,
-    counts: locals.counts
+    counts: locals.counts,
+    origin: url.origin
   };
 }
