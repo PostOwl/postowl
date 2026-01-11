adef15e59d3cfdfd899bf12355e1b9af562b5dba
Tom Atkins <tom.atkins@neptune.ai>
2026-01-11 21:55:08 +0000

    Change format of displayed post dates to yyyy-mm-dd

--- src/lib/util.js
+++ src/lib/util.js
@@ -89,11 +89,10 @@ export function formatDate(dateString, withTime) {
       return date.toLocaleDateString('en-US', opts);
     }
   } else {
-    return date.toLocaleDateString('en-US', {
-      month: 'short',
-      day: 'numeric',
-      year: 'numeric'
-    });
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
   }
 }
 
